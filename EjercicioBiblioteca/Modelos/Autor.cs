a67eef03e15ceeaf6327777bb2600666a07d9d70
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-14 19:42:23 -0600

    propiedades de las tablas

---
+++ EjercicioBiblioteca/Modelos/Autor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EjercicioBiblioteca.Modelos
+{
+    public class Autor
+    {
+        [Key]
+        public int id_Autor { get; set; }
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+
+    }
+}
---
+++ EjercicioBiblioteca/Modelos/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EjercicioBiblioteca.Modelos
+{
+    public class Libro
+    {
+        [Key]
+        public int id_libro { get; set; }
+        public string titulo { get; set; }
+        public int? anioPublicacion { get; set; }
+        public int? autor_id { get; set; }
+        public int? categoria_id { get; set; }
+        public string resumen { get; set; }
+    }
+}
