7b618a2dfdfb4776c39afeb36509d6698453530d
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-14 19:53:35 -0600

    update

--- EjercicioBiblioteca/Program.cs
+++ EjercicioBiblioteca/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Modelos;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BibliotecaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BibliotecaDbConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
