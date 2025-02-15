1245dd799ddbc9a9ede6c82a26afed1fa3ce2057
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-14 21:51:02 -0600

    Libro controller y ver todos los registro

---
+++ EjercicioBiblioteca/Controllers/LibroController.cs
@@ -0,0 +1,33 @@
+﻿using EjercicioBiblioteca.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EjercicioBiblioteca.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly BibliotecaContext _BibliotecaContext;
+
+        public LibroController(BibliotecaContext BibliotecaContext)
+        {
+            _BibliotecaContext = BibliotecaContext;
+        }
+
+        // Para poder ver todos los registros:
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Index()
+        {
+            List<Libro> listadoLibro = (from e in _BibliotecaContext.Libro select e).ToList();
+
+            if (listadoLibro.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoLibro);
+        }
+    }
+}
