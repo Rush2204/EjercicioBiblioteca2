1f0faf0f0584ce9be2e70227f30cef7cc7e4210b
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-14 19:43:35 -0600

    bibliotecaContext

---
+++ EjercicioBiblioteca/Modelos/BibliotecaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace WebApplication1.Modelos
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+        }
+
+    }
+}
